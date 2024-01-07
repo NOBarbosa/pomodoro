499a1e274ffb918633990c74beb853637ddcfa26
Natalia Barbosa <natalia.barbosa@zig.fun>
2024-01-07 17:35:27 -0300

    feat: configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div>POMODORO</div>
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
--- src/assets/@types/styles.d.ts
+++ src/assets/@types/styles.d.ts
@@ -2,3 +2,7 @@ import "styled-components";
 import { defaultTheme } from "../../styles/themes/default";
 
 type theameType = typeof defaultTheme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}
